8f9caef2ae3f98a75311bf33daee0c91820415d5
ReallPanda <douglashansenjr@gmail.com>
2023-04-21 20:36:39 -0700

    added caching

--- src/lib/cacheHelpers.ts
+++ src/lib/cacheHelpers.ts
@@ -1,10 +1,17 @@
 import { Member, Server } from '../schema.js';
-import { supabase } from '../main.js';
+import { cache, supabase } from '../main.js';
 
 export async function getMember(
   guild_id: string,
   member_id: string
 ): Promise<Member> {
+  const cacheResult = cache.get<Member>(`member:${guild_id}-${member_id}`);
+
+  if (cacheResult) {
+    console.log('📦🔽 Member');
+    return cacheResult;
+  }
+
   const result = await supabase
     .from('member')
     .select()
@@ -24,13 +31,26 @@ export async function getMember(
     if (newMember.error)
       throw new Error(`Error inserting memeber from Supabase:\n ${newMember}`);
 
+    console.log('⚡🔽 Making new Member');
+
+    cache.set(`member:${guild_id}-${member_id}`, newMember.data[0]);
     return newMember.data[0];
   }
 
+  console.log('⚡🔽 Member');
+
+  cache.set(`member:${guild_id}-${member_id}`, result.data[0]);
   return result.data[0];
 }
 
 export async function getServer(guild_id: string): Promise<Server> {
+  const cacheResult = cache.get<Server>(`server:${guild_id}`);
+
+  if (cacheResult) {
+    console.log('📦🔽 Server');
+    return cacheResult;
+  }
+
   const result = await supabase
     .from('server')
     .select()
@@ -49,9 +69,15 @@ export async function getServer(guild_id: string): Promise<Server> {
     if (newServer.error)
       throw new Error(`Error inserting server from Supabase:\n ${newServer}`);
 
+    console.log('⚡🔽 Making new Server');
+
+    cache.set(`server:${guild_id}`, newServer.data[0]);
     return newServer.data[0];
   }
 
+  console.log('⚡🔽 Server');
+
+  cache.set(`server:${guild_id}`, result.data[0]);
   return result.data[0];
 }
 
@@ -60,22 +86,28 @@ export async function setMember(
   member_id: string,
   data: Member
 ) {
-  const update = await supabase
-    .from('member')
-    .update(data)
-    .eq('server_id', guild_id)
-    .eq('member_id', member_id);
+  // const update = await supabase
+  //   .from('member')
+  //   .update(data)
+  //   .eq('server_id', guild_id)
+  //   .eq('member_id', member_id);
 
-  if (update.error)
-    throw new Error(`Error updating Member in Supabase:\n ${update}`);
+  // if (update.error)
+  //   throw new Error(`Error updating Member in Supabase:\n ${update}`);
+
+  console.log('📦🔼 Member');
+  cache.set(`member:${guild_id}-${member_id}`, data);
 }
 
 export async function setServer(guild_id: string, data: Server) {
-  const update = await supabase
-    .from('server')
-    .update(data)
-    .eq('server_id', guild_id);
+  // const update = await supabase
+  //   .from('server')
+  //   .update(data)
+  //   .eq('server_id', guild_id);
 
-  if (update.error)
-    throw new Error(`Error updating Server in Supabase:\n ${update}`);
+  // if (update.error)
+  //   throw new Error(`Error updating Server in Supabase:\n ${update}`);
+
+  console.log('📦🔼 Server');
+  cache.set(`server:${guild_id}`, data);
 }
--- src/lib/index.ts
+++ src/lib/index.ts
@@ -2,3 +2,4 @@ export * from './cacheHelpers.js';
 export * from './isMod.js';
 export * from './modLog.js';
 export * from './userRoles.js';
+export * from './assertions.js';
