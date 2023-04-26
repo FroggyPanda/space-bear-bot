b0ba2c6f1c7322dcd4743876975645ff02d4484d
ReallPanda <101910741+FroggyPanda@users.noreply.github.com>
2023-04-25 18:51:53 -0700

    fixed schema

--- src/schema.ts
+++ src/schema.ts
@@ -4,118 +4,123 @@ export type Json =
   | boolean
   | null
   | { [key: string]: Json }
-  | Json[]
+  | Json[];
+
+type LevelRole = {
+  role_id: string;
+  level: number;
+};
 
 export interface Database {
   public: {
     Tables: {
       member: {
         Row: {
-          id: number
-          last_message_timestamp: number
-          last_pat_timestamp: number
-          level: number
-          member_id: string
-          message: number
-          pat: number
-          server_id: string
-          xp: number
-        }
+          id: number;
+          last_message_timestamp: number;
+          last_pat_timestamp: number;
+          level: number;
+          member_id: string;
+          message: number;
+          pat: number;
+          server_id: string;
+          xp: number;
+        };
         Insert: {
-          id?: number
-          last_message_timestamp?: number
-          last_pat_timestamp?: number
-          level?: number
-          member_id: string
-          message?: number
-          pat?: number
-          server_id: string
-          xp?: number
-        }
+          id?: number;
+          last_message_timestamp?: number;
+          last_pat_timestamp?: number;
+          level?: number;
+          member_id: string;
+          message?: number;
+          pat?: number;
+          server_id: string;
+          xp?: number;
+        };
         Update: {
-          id?: number
-          last_message_timestamp?: number
-          last_pat_timestamp?: number
-          level?: number
-          member_id?: string
-          message?: number
-          pat?: number
-          server_id?: string
-          xp?: number
-        }
-      }
+          id?: number;
+          last_message_timestamp?: number;
+          last_pat_timestamp?: number;
+          level?: number;
+          member_id?: string;
+          message?: number;
+          pat?: number;
+          server_id?: string;
+          xp?: number;
+        };
+      };
       server: {
         Row: {
-          id: number
-          level_message_channel: string | null
-          level_message_channels: string[]
-          level_ranks: Json[]
-          mod_id: string | null
-          mod_log_channel: string | null
-          pat: number
-          server_id: string
-        }
+          id: number;
+          level_message_channel: string | null;
+          level_message_channels: string[];
+          level_ranks: LevelRole[];
+          mod_id: string | null;
+          mod_log_channel: string | null;
+          pat: number;
+          server_id: string;
+        };
         Insert: {
-          id?: number
-          level_message_channel?: string | null
-          level_message_channels?: string[]
-          level_ranks?: Json[]
-          mod_id?: string | null
-          mod_log_channel?: string | null
-          pat?: number
-          server_id: string
-        }
+          id?: number;
+          level_message_channel?: string | null;
+          level_message_channels?: string[];
+          level_ranks?: LevelRole[];
+          mod_id?: string | null;
+          mod_log_channel?: string | null;
+          pat?: number;
+          server_id: string;
+        };
         Update: {
-          id?: number
-          level_message_channel?: string | null
-          level_message_channels?: string[]
-          level_ranks?: Json[]
-          mod_id?: string | null
-          mod_log_channel?: string | null
-          pat?: number
-          server_id?: string
-        }
-      }
+          id?: number;
+          level_message_channel?: string | null;
+          level_message_channels?: string[];
+          level_ranks?: LevelRole[];
+          mod_id?: string | null;
+          mod_log_channel?: string | null;
+          pat?: number;
+          server_id?: string;
+        };
+      };
       warning: {
         Row: {
-          id: string
-          member_id: string
-          mod_id: string
-          reason: string
-          server_id: string
-          timestamp: number
-        }
+          id: string;
+          member_id: string;
+          mod_id: string;
+          reason: string;
+          server_id: string;
+          timestamp: number;
+        };
         Insert: {
-          id?: string
-          member_id: string
-          mod_id: string
-          reason: string
-          server_id: string
-          timestamp: number
-        }
+          id?: string;
+          member_id: string;
+          mod_id: string;
+          reason: string;
+          server_id: string;
+          timestamp: number;
+        };
         Update: {
-          id?: string
-          member_id?: string
-          mod_id?: string
-          reason?: string
-          server_id?: string
-          timestamp?: number
-        }
-      }
-    }
+          id?: string;
+          member_id?: string;
+          mod_id?: string;
+          reason?: string;
+          server_id?: string;
+          timestamp?: number;
+        };
+      };
+    };
     Views: {
-      [_ in never]: never
-    }
+      [_ in never]: never;
+    };
     Functions: {
-      [_ in never]: never
-    }
+      [_ in never]: never;
+    };
     Enums: {
-      [_ in never]: never
-    }
+      [_ in never]: never;
+    };
     CompositeTypes: {
-      [_ in never]: never
-    }
-  }
+      [_ in never]: never;
+    };
+  };
 }
 
 export type Member = Database['public']['Tables']['member']['Row'];
