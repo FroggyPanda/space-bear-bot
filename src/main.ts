4c6e25ba9fcdfeced1bd44cbb6aee68f0dbbaa08
ReallPanda <101910741+FroggyPanda@users.noreply.github.com>
2023-04-27 21:14:18 -0700

    feat: caching

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,46 @@ import { Database } from 'schema.js';
 import Log from './decorators/Log.js';
 import chalk from 'chalk';
 import boxen from 'boxen';
+import NodeCache from 'node-cache';
+
+export type CacheMember = {
+  id: number;
+  message: number;
+  xp: number;
+  level: number;
+  last_message_timestamp: number;
+};
+
+process.on('uncaughtException', (error) => {
+  console.error(error);
+
+  bot.users.send(
+    '188851079050428427',
+    `${bot.user?.username} has experienced a bug\n \`\`\`${error}\`\`\``
+  );
+
+  cache.keys().forEach(async (key: string) => {
+    const value = cache.get<CacheMember>(key);
+    const tableName = key.split(':')[0];
+
+    if (tableName === 'member') {
+      if (value) {
+        const result = await supabase
+          .from('member')
+          .update({
+            message: value.message,
+            xp: value.xp,
+            level: value.level,
+            last_message_timestamp: value.last_message_timestamp,
+          })
+          .eq('id', value.id);
+
+        if (result.error)
+          throw new Error(`Error updating Supabase from cache:\n${result}`);
+      }
+    }
+  });
+});
 
 config();
 
@@ -24,6 +64,28 @@ export const supabase = createClient<Database>(
   process.env.SUPABASE_URL,
   process.env.SUPABASE_SERVICE_ROLE
 );
+
+// All in seconds
+export const cache = new NodeCache({ stdTTL: 30 * 60, checkperiod: 10 * 60 });
+
+cache.on('del', async (key: string, value: CacheMember) => {
+  const tableName = key.split(':')[0];
+
+  if (tableName === 'member') {
+    const result = await supabase
+      .from('member')
+      .update({
+        message: value.message,
+        xp: value.xp,
+        level: value.level,
+        last_message_timestamp: value.last_message_timestamp,
+      })
+      .eq('id', value.id);
+
+    if (result.error)
+      throw new Error(`Error updating Supabase from cache:\n${result}`);
+  }
+});
 
 const ytdlPlayerPlugin = new YTDLPlayerPlugin({
   metadata: MetadataStorage.instance,
